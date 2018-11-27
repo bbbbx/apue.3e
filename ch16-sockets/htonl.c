71f53e0ee6c27da95c319e6083a38ba7b816c2e8
venus <venus@venusworld.cn>
2018-11-27 17:39:35 +0800

    update ch16

--- ch16-sockets/htonl.c
+++ ch16-sockets/htonl.c
@@ -2,8 +2,10 @@
 #include <arpa/inet.h>
 
 int main(void) {
-  uint32_t hostint32= 0x0101a8c0;        /* 1.1.168.192 */
-  uint32_t netint32 = htonl(hostint32);   /* 192.168.1.1 */
+  uint32_t hostint32= 0xc0a80101;         /* 192.168.1.1 */
+  uint32_t netint32 = htonl(hostint32);   /* 1.1.168.192 */
+  char     addr[15];
+  inet_ntop(AF_INET, &netint32, addr, sizeof(addr));
 
-  printf("hostint32: %#x\nnetint32: %#x\n", hostint32, netint32);
+  printf("hostint32: %#x\nnetint32: %#x\ninet_ntop: %s\n", hostint32, netint32, addr);
 }
